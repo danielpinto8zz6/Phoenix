9570e653a37ae572138d3707962802f96793c263
danielpinto8zz6 <danielpinto8zz6@gmail.com>
2018-05-10 17:45:46 +0100

    Fix memory

--- phoenix_server/game.cpp
+++ phoenix_server/game.cpp
@@ -1,17 +1,18 @@
 #include "stdafx.h"
+
 #include "game.h"
 
 Coordinates *GetFirstEmptyPosition(Game *game) {
-	Coordinates coordinates;
+  Coordinates coordinates;
 
-	for (int x = 0; x < HEIGHT; x++) {
-		for (int y = 0; y < WIDTH; y++) {
-			if (game->map[x][y] == ' ' || game->map[x][y] == '\0') {
-				coordinates.x = x;
-				coordinates.y = y;	
-				return &coordinates;
-			}
-		}
-	}
-	return NULL;
+  for (int x = 0; x < WIDTH; x++) {
+    for (int y = 0; y < HEIGHT; y++) {
+      if (game->map[x][y] == ' ' || game->map[x][y] == '\0') {
+        coordinates.x = x;
+        coordinates.y = y;
+        return &coordinates;
+      }
+    }
+  }
+  return NULL;
 }
