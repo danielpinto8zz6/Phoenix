5e06eab1745bf2261016c6c11a2c5971aec2bcab
danielpinto8zz6 <danielpinto8zz6@gmail.com>
2018-06-19 14:48:11 +0100

    Check overlapp & point inside rect

--- PhoenixServer/Game.h
+++ PhoenixServer/Game.h
@@ -1,8 +1,9 @@
 ﻿#pragma once
 
-Coordinates getFirstEmptyPosition(Game *game);
-BOOL isPositionOccupied(Game *game, Coordinates coordinates);
 BOOL coordinatesEqual(Coordinates c1, Coordinates c2);
+Coordinates getFirstEmptyPosition(Game *game);
 BOOL isCoordinatesValid(Coordinates coordinates);
+BOOL isPositionOccupied(Game *game, Coordinates coordinates);
+BOOL isRectangleOverlapping(Coordinates rect1Coord, Size rect1Sz, Coordinates rect2Coord, Size rect2Sz);
 DWORD WINAPI threadEnemyShip(LPVOID lpParam);
 DWORD WINAPI threadManageEnemyShips(LPVOID lpParam);
