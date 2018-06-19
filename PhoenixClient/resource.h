53123c1f46f1e5ad07f1b53f9eee2d2f59851cdd
tiago rodrigues <tiago_r_0694@hotmail.com>
2018-06-19 18:45:58 +0100

    bitmaps

--- PhoenixClient/resource.h
+++ PhoenixClient/resource.h
@@ -1,6 +1,6 @@
 ﻿//{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by PhoenixClient.rc
+// Arquivo de inclusão gerado pelo Microsoft Visual C++.
+// Usado por PhoenixClient.rc
 //
 #define IDC_MYICON                      2
 #define IDD_PHOENIXCLIENT_DIALOG        102
@@ -14,7 +14,11 @@
 #define IDC_PHOENIXCLIENT               109
 #define IDR_MAINFRAME                   128
 #define IDD_DIALOG_MENU                 130
-#define IDB_BITMAP1                     131
+#define IDB_BITMAP_BOMBS                131
+#define IDB_BITMAP_POWERUP_ICE          133
+#define IDB_BITMAP_DEFENDERS            134
+#define IDB_BITMAP_ENEMY_SHIPS          135
+#define IDB_BITMAP_ENEMY_SHIPS_BASIC    135
 #define IDC_EDIT1                       1000
 #define IDC_EDIT_USERNAME               1000
 #define IDC_BUTTON1                     1001
@@ -30,7 +34,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_RESOURCE_VALUE        136
 #define _APS_NEXT_COMMAND_VALUE         32772
 #define _APS_NEXT_CONTROL_VALUE         1006
 #define _APS_NEXT_SYMED_VALUE           110
